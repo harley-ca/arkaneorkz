8160166c4e3765ae4c8dee0bf068a8e7e7cb28e2
harley-ca <gcab022110@coderacademy.edu.au>
2021-12-14 21:42:28 +1000

    changed weapon class, added knife example

--- src/weapon.rb
+++ src/weapon.rb
@@ -1,26 +1,30 @@
-class weapon
-    def initialize(weapon_name, enchant, description, stats, attack_desc)
+class Weapon
+    attr_reader :description
+    def initialize(weapon_name, attack_bonus, damage_calc, damage, hands, enchant, lootable, description, attack_style)
         @weapon_name = weapon_name
-        @stats = stats #atk bonus, damage, hands, stat, lootable
+        @attack_bonus = attack_bonus
+        @damage_calc = damage_calc
+        @damage = damage
+        @hands = hands
         @enchant = enchant
+        @lootable = lootable
         @description = description
-        @attack_desc = attack_desc
+        @attack_style = attack_style
     end
 
-    def damage()
-        #damage + enchant damage
-        dmg = 0
-        if enchant #add enchant if relevent
-            #enchant effect
-        end
-
-        if #stats[damage] = 1d6
-            return rand(1..6)
-        elsif #stats = 1d8
-            return rand(1..8)
-        elsif # 1d10
-            return rand(1..10)
-        end
+    def to_s
+        print "#{@weapon_name} | +#{@attack_bonus} | #{@damage} | #{@hands}h"
     end
 
+    def about
+        print "#{@weapon_name}, #{@description}, #{@enchant}"
+    end
+
+    def damage
+        dmg = eval @damage_calc
+        return dmg
+    end
 end
+
+knife = Weapon.new("Knife", 3, "rand(1..6)", "1d6", 1, "", true, "A small sharp knife.", "stabbing")
+
